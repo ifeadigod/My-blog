dcf2a4518955bf50600161d1bcc60833f2b498aa
Ifeadigo Udeh <ifeadigod69@gmail.com>
2022-05-30 13:25:36 +0100

    db module

--- my_work/my_work/settings.py
+++ my_work/my_work/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,10 +33,18 @@ if environment =='development':
 
     DEBUG = True
     ALLOWED_HOSTS =[]
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'dbsqlite3',
+        }
+    }
 
 elif environment == 'production':
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['ifeblog.herokuapp.com']
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -92,12 +101,6 @@ WSGI_APPLICATION = 'my_work.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
